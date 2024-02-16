8e5ca3ce23ffa8673182f02b07019d9c86583486
Will-Key <saibe.akreiry@gmail.com>
2024-02-16 22:27:41 +0000

    Fixed Navbar and add a big margin top on Home Page

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,6 +1,6 @@
 export default function Navbar() {
   return (
-    <header className="flex items-center top-0 left-0 bg-white z-50 shadow-lg px-10 h-20 w-full mb-10">
+    <header className="fixed flex items-center top-0 left-0 bg-white z-50 shadow-lg px-10 h-20 w-full">
       <div className="flex items-center justify-between w-full">
         <p className="text-lg font-bold text-black ">Where in the world</p>
       </div>
--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -6,7 +6,7 @@ export default function HomePage() {
   return (
     <div>
       <Navbar />
-      <div className="m-10 flex flex-col gap-6">
+      <div className="mt-28 ml-10 mr-10 mb-10 flex flex-col gap-6">
         <Filter />
         <CountriesList />
       </div>
