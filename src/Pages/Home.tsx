b3776bd98235b41c1841f8e25ed168e8531569b5
Will-Key <saibe.akreiry@gmail.com>
2024-02-17 15:13:05 +0000

    Update Home page design

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,15 +1,11 @@
 import CountriesList from '../Components/CoutriesList'
 import Filter from '../Components/Filter'
-import Navbar from '../Components/Navbar'
 
 export default function HomePage() {
   return (
-    <div>
-      <Navbar />
-      <div className="mt-28 ml-10 mr-10 mb-10 flex flex-col gap-6">
-        <Filter />
-        <CountriesList />
-      </div>
+    <div className="flex flex-col gap-6">
+      <Filter />
+      <CountriesList />
     </div>
   )
 }
