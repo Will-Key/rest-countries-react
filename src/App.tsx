cc0d9b0288128529f22b06cc940edabb33336b24
Will-Key <saibe.akreiry@gmail.com>
2024-02-17 14:44:16 +0000

    Add a new route for country infos

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,26 @@
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import HomePage from './Pages/Home'
+import CountryPage from './Pages/Country'
+import Navbar from './Components/Navbar'
 
 const router = createBrowserRouter([
   {
     path: '',
     element: <HomePage />,
   },
+  {
+    path: '/:countryName',
+    element: <CountryPage />,
+  },
 ])
 
 function App() {
-  return <RouterProvider router={router} />
+  return (
+    <div className="mt-28 ml-14 mr-14 mb-14">
+      <Navbar />
+      <RouterProvider router={router} />
+    </div>
+  )
 }
 
 export default App
