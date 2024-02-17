4aa2b0b56bb58302ab00eef95e82ed0f4d854e77
Will-Key <saibe.akreiry@gmail.com>
2024-02-17 14:45:29 +0000

    Update file name

---
+++ src/Components/SearchInput.tsx
@@ -0,0 +1,26 @@
+import { ReactNode } from 'react'
+
+type PROPS = {
+  placeholder: string
+  icon?: ReactNode
+  value?: string
+  onChange?: React.ChangeEventHandler<HTMLInputElement> | undefined
+}
+
+export default function SearchInput(props: PROPS) {
+  return (
+    <div className="relative">
+      <input
+        className="placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
+        type="text"
+        name="search"
+        placeholder={props.placeholder}
+        onChange={props.onChange}
+        value={props.value}
+      />
+      <div className="absolute inset-y-0 left-0 flex items-center pl-3">
+        {props.icon}
+      </div>
+    </div>
+  )
+}
