4d1a5df2866061980408a60edbd77d142bc7e8c3
Will-Key <saibe.akreiry@gmail.com>
2024-02-17 15:10:02 +0000

    Update Card design

--- src/Components/Card.tsx
+++ src/Components/Card.tsx
@@ -1,6 +1,31 @@
-export default function Card() {
+import { Link } from 'react-router-dom'
+
+type Country = {
+  name: {
+    common: string
+    official: string
+  }
+  capital: string[]
+  population: number
+  timezones: string[]
+  region: string
+  subregion: string
+  continents: string[]
+  languages: { [key: string]: string }
+  flags: { png: string; svg: string; alt: string }
+}
+
+type PROPS = {
+  country: Country
+}
+
+export default function Card(props: PROPS) {
+  // ${props.country.name.common}
+  // transition-transform duration-300 transform hover:scale-110
   return (
-    <div className="bg-white w-full shadow-lg transition-transform duration-300 transform hover:scale-110">
+    <Link
+      to={`/senegal`}
+      className="bg-white w-full shadow-lg transform duration-300 hover:-translate-y-3 hover:cursor-pointer">
       <div className="w-full">
         <img
           src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAOEAAACWCAMAAAAfSh8xAAAACVBMVEX3fwD///8AnmBWUWjSAAAAoUlEQVR4nO3PMQEAAAgDoGn/0IZwJzQg6Zme7SkGDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDR8OvdOD1wQ8tBkAAAAASUVORK5CYII="
@@ -10,16 +35,20 @@ export default function Card() {
         />
       </div>
       <div className="my-4 px-4">
+        <p className="font-bold text-lg my-5">Côte d'Ivoire</p>
         <p>
-          Pays: <span className="font-bold text-lg">Côte d'Ivoire</span>
+          <span className="text-md font-semibold">Population: </span>
+          <span className="text-md">28.000.000</span>
         </p>
         <p>
-          Capital: <span className="font-bold text-lg">Yamoussoukro</span>
+          <span className="text-md font-semibold">Region: </span>
+          <span className="text-md">Africa</span>
         </p>
         <p>
-          Population: <span className="font-bold text-lg">28.000.000</span>
+          <span className="text-md font-semibold">Capital: </span>
+          <span className="text-md">Yamoussoukro</span>
         </p>
       </div>
-    </div>
+    </Link>
   )
 }
