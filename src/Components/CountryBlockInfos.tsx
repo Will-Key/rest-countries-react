d7293011edc6231ce1973d1ef8f4e65122af2daa
Will-Key <saibe.akreiry@gmail.com>
2024-02-21 03:48:27 +0000

    Display data in CountryBlockInfos component

--- src/Components/CountryBlockInfos.tsx
+++ src/Components/CountryBlockInfos.tsx
@@ -1,16 +1,21 @@
+import { Country } from '../types'
 import CountryBlockInfosBorders from './CountryBlockInfosBorders'
 import CountryBlockInfosSectionOne from './CountryBlockInfosSectionOne'
 import CountryBlockInfosSectionTwo from './CountryBlockInfosSectionTwo'
 
-export default function CountryBlockInfos() {
+type PROPS = {
+  country?: Country
+}
+
+export default function CountryBlockInfos({ country }: PROPS) {
   return (
     <div className="flex flex-col">
-      <h2 className="my-4 font-bold text-4xl">Ivory Coast</h2>
+      <h2 className="my-4 font-bold text-4xl">{country?.name}</h2>
       <div className="flex flex-col md:flex-row gap-36">
-        <CountryBlockInfosSectionOne />
-        <CountryBlockInfosSectionTwo />
+        <CountryBlockInfosSectionOne country={country} />
+        <CountryBlockInfosSectionTwo country={country} />
       </div>
-      <CountryBlockInfosBorders />
+      <CountryBlockInfosBorders borders={country?.borders} />
     </div>
   )
 }
