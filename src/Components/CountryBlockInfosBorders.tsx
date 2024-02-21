0d0735efc6428ec04925e8926cf4d8c452af58a8
Will-Key <saibe.akreiry@gmail.com>
2024-02-21 03:52:03 +0000

    Display infomation about country selected borders and adding animation on component

--- src/Components/CountryBlockInfosBorders.tsx
+++ src/Components/CountryBlockInfosBorders.tsx
@@ -1,18 +1,48 @@
+import { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
+import { apiFetch } from '../utils/api-fetch'
+import { Country } from '../types'
+import { AnimatePresence, motion } from 'framer-motion'
 
-export default function CountryBlockInfosBorders() {
+type PROPS = {
+  borders?: string[]
+}
+
+export default function CountryBlockInfosBorders({ borders }: PROPS) {
+  const [countries, setCountries] = useState<Country[]>([])
+
+  useEffect(() => {
+    if (borders?.length) {
+      apiFetch<Country[]>({ urlPath: `alpha?codes=${borders.join()}` }).then(
+        (countries) => {
+          return setCountries(countries)
+        }
+      )
+    }
+  }, [borders, countries])
+
   return (
     <div className="mt-10">
       <span className="my-4 mr-4 font-semibold text-base">
-        Border Countries:{' '}
+        Border Countries:
       </span>
-      {[1, 2, 3, 4, 5, 1, 3, 5].map((_) => (
-        <Link
-          to="/ghana"
-          className="inline-block mr-4 my-1 px-4 py-1.5 shadow-md">
-          Burkina Faso
-        </Link>
+      {countries.map((country) => (
+        <AnimatePresence>
+          <motion.div
+            initial={{ y: -30, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            exit={{ y: -10, opacity: 0 }}
+            transition={{ duration: 0.8 }}
+            className="inline-block mr-4 my-1 px-4 py-1.5 shadow-md">
+            <Link key={country.name} to={`/${country.name}`}>
+              {country.name}
+            </Link>
+          </motion.div>
+        </AnimatePresence>
       ))}
+      {!borders?.length && (
+        <span className="text-black text-base">No borders</span>
+      )}
     </div>
   )
 }
