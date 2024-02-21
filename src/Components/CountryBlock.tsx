9b33278a9dc9f65dfcf17c5f574761162f1b9c36
Will-Key <saibe.akreiry@gmail.com>
2024-02-21 03:46:58 +0000

    Use motion.div instead of basic div and add animation on CountryBlock component

--- src/Components/CountryBlock.tsx
+++ src/Components/CountryBlock.tsx
@@ -1,16 +1,22 @@
 import { Country } from '../types'
 import CountryBlockImage from './CountryBlockImage'
 import CountryBlockInfos from './CountryBlockInfos'
+import { motion } from 'framer-motion'
 
 type PROPS = {
-  country: Country
+  country?: Country
 }
 
-export default function CountryBlock() {
+export default function CountryBlock({ country }: PROPS) {
   return (
-    <div className="mt-5 grid grid-cols-1 md:grid-cols-2 gap-10">
-      <CountryBlockImage />
-      <CountryBlockInfos />
-    </div>
+    <motion.div
+      initial={{ x: -50, opacity: 0 }}
+      animate={{ x: 0, opacity: 1 }}
+      exit={{ x: -10, opacity: 0 }}
+      transition={{ duration: 0.7 }}
+      className="mt-5 grid grid-cols-1 md:grid-cols-2 gap-10">
+      <CountryBlockImage src={country?.flag} alt={country?.name} />
+      <CountryBlockInfos country={country} />
+    </motion.div>
   )
 }
