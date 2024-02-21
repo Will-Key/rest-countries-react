22bf299fa0ca9ac0ed2667b176488ae4f19acb56
Will-Key <saibe.akreiry@gmail.com>
2024-02-21 03:53:41 +0000

    Display content of country selected in CountryBlockInfosSectionOne and CountryBlockInfosSectionTwo

--- src/Components/CountryBlockInfosSectionOne.tsx
+++ src/Components/CountryBlockInfosSectionOne.tsx
@@ -1,22 +1,31 @@
-export default function CountryBlockInfosSectionOne() {
+import { Country } from '../types'
+
+type PROPS = {
+  country?: Country
+}
+
+export default function CountryBlockInfosSectionOne({ country }: PROPS) {
   return (
     <div>
       <p className="my-4">
-        <span className="font-semibold text-base">Native Name:</span>Côte
-        d'ivoire
+        <span className="font-semibold text-base">Native Name: </span>
+        {country?.nativeName}
       </p>
       <p className="my-4">
-        <span className="font-semibold text-base">Population: </span>26 378 275
+        <span className="font-semibold text-base">Population: </span>
+        {country?.population}
       </p>
       <p className="my-4">
-        <span className="font-semibold text-base">Region: </span>Africa
+        <span className="font-semibold text-base">Region: </span>
+        {country?.region}
       </p>
       <p className="my-4">
-        <span className="font-semibold text-base">Sub Region: </span>Western
-        Africa
+        <span className="font-semibold text-base">Sub Region: </span>
+        {country?.subregion}
       </p>
       <p className="my-4">
-        <span className="font-semibold text-base">Capital:</span>Yamoussoukro
+        <span className="font-semibold text-base">Capital: </span>
+        {country?.capital}
       </p>
     </div>
   )
--- src/Components/CountryBlockInfosSectionTwo.tsx
+++ src/Components/CountryBlockInfosSectionTwo.tsx
@@ -1,15 +1,23 @@
-export default function CountryBlockInfosSectionTwo() {
+import { Country } from '../types'
+
+type PROPS = {
+  country?: Country
+}
+
+export default function CountryBlockInfosSectionTwo({ country }: PROPS) {
   return (
     <div>
       <p className="my-4">
-        <span className="font-semibold text-base">Top Level Domain:</span>.ci
+        <span className="font-semibold text-base">Top Level Domain: </span>
+        {country?.topLevelDomain}
       </p>
       <p className="my-4">
-        <span className="font-semibold text-base">Currencies: </span>West
-        African CFA franc
+        <span className="font-semibold text-base">Currencies: </span>
+        {country?.currencies?.map((curr) => curr.name).join()}
       </p>
       <p className="my-4">
-        <span className="font-semibold text-base">Languages: </span>French
+        <span className="font-semibold text-base">Languages: </span>
+        {country?.languages?.map((lang) => lang.name).join()}
       </p>
     </div>
   )
