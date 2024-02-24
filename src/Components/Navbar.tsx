57fa2f809b1c5c67d0159842c1b4c152606a2b33
Will-Key <saibe.akreiry@gmail.com>
2024-02-24 04:43:04 +0000

    Increase Navbar pading

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,6 +1,6 @@
 export default function Navbar() {
   return (
-    <header className="fixed flex items-center top-0 left-0 bg-white z-50 shadow-lg px-10 h-20 w-full">
+    <header className="fixed flex items-center top-0 left-0 bg-white z-50 shadow-lg px-14 h-20 w-full">
       <div className="flex items-center justify-between w-full">
         <p className="text-lg font-bold text-black ">Where in the world</p>
       </div>
